be72ab01b80dd92ec91a9eebed7f01f2134628e5
Victor Strand <92252033+Vss86@users.noreply.github.com>
2023-11-28 15:29:11 +0100

    Football Logos Memory

---
+++ footballlogos/src/Game/Data.js
@@ -0,0 +1,87 @@
+const Data = [ 
+	{ 
+		id: 1, 
+		name: "FC Barcelona", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 2, 
+		name: "FC Bayern", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg/2048px-FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 3, 
+		name: "Arsenal FC", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/hif/8/82/Arsenal_FC.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 4, 
+		name: "Olympique Lyonnaise", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Olympique_Lyonnais_logo.svg/1200px-Olympique_Lyonnais_logo.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 5, 
+		name: "Juventus", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juventus_FC_2017_icon_%28black%29.svg/486px-Juventus_FC_2017_icon_%28black%29.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 6, 
+		name: "Panathinaikos FC", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Panathinaikos_F.C._logo.svg/1200px-Panathinaikos_F.C._logo.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 7, 
+		name: "FC Barcelona", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/1200px-FC_Barcelona_%28crest%29.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 8, 
+		name: "FC Bayern", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg/2048px-FC_Bayern_M%C3%BCnchen_logo_%282017%29.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 9, 
+		name: "Arsenal FC", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/hif/8/82/Arsenal_FC.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 10, 
+		name: "Olympique Lyonnaise", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Olympique_Lyonnais_logo.svg/1200px-Olympique_Lyonnais_logo.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 11, 
+		name: "Juventus", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juventus_FC_2017_icon_%28black%29.svg/486px-Juventus_FC_2017_icon_%28black%29.svg.png", 
+		matched: false, 
+	}, 
+	{ 
+		id: 12, 
+		name: "Panathinaikos FC", 
+		img: 
+"https://upload.wikimedia.org/wikipedia/en/thumb/8/84/Panathinaikos_F.C._logo.svg/1200px-Panathinaikos_F.C._logo.svg.png", 
+		matched: false, 
+	}, 
+]; 
+export default Data;
